bc3245c8f15447c78c42c1989101b1ac631949ba
urkejov1996 <urkejov1996@gmail.com>
2024-03-11 14:50:34 +0100

    added update todo feature and connect it to backend api

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,8 +1,8 @@
-import { useParams } from "react-router-dom";
-import { retrieveTodoApi } from "./api/TodoApiService";
+import { useParams, useNavigate } from "react-router-dom";
+import { retrieveTodoApi, updateTodoApi } from "./api/TodoApiService";
 import { useAuth } from "./security/AuthContext";
 import { useEffect, useState } from "react";
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 
 export default function TodoComponent() {
   const { id } = useParams();
@@ -11,6 +11,8 @@ export default function TodoComponent() {
   const [targetDate, setTargetDate] = useState("");
 
   const authContext = useAuth();
+  const navigate = useNavigate();
+
   const username = authContext.username;
 
   useEffect(() => retrieveTodos(), [id]);
@@ -26,6 +28,34 @@ export default function TodoComponent() {
 
   function onSubmit(values) {
     console.log(values);
+
+    const todo = {
+      id: id,
+      username: username,
+      description: values.description,
+      targetDate: values.targetDate,
+      done: false,
+    };
+    updateTodoApi(username, id, todo)
+    .then((response) => {
+        navigate('/todos')
+      })
+      .catch((error) => console.log(error));
+  }
+  function validate(values) {
+    let errors = {
+      //   description: "Enter a valid description",
+      //   targetDate:"Enter a valid target date"
+    };
+
+    if (values.description.length < 5) {
+      errors.description = "Enter minimum 5 characters.";
+    }
+    if (values.targetDate == null) {
+      errors.description = "Enter a target date..";
+    }
+    console.log(values);
+    return errors;
   }
 
   return (
@@ -36,9 +66,22 @@ export default function TodoComponent() {
           initialValues={{ description, targetDate }}
           enableReinitialize={true}
           onSubmit={onSubmit}
+          validate={validate}
+          validateOnChange={false}
+          validateOnBlur={false}
         >
           {(props) => (
             <Form>
+              <ErrorMessage
+                name="description"
+                component="div"
+                className="alert alert-warning"
+              />
+              <ErrorMessage
+                name="targetDate"
+                component="div"
+                className="alert alert-warning"
+              />
               <fieldset className="form-group">
                 <label>Description</label>
                 <Field
--- src/components/todo/api/TodoApiService.jsx
+++ src/components/todo/api/TodoApiService.jsx
@@ -1,18 +1,21 @@
-import axios from "axios"
+import axios from "axios";
 
 // export function retrieveHelloWorldBean() {
 //     return axios.get('http://localhost:8080/hello-world-bean')
 // }
 
-const apiClient = axios.create(
-    {
-        baseURL: 'http://localhost:8080'
-    }
-)
+const apiClient = axios.create({
+  baseURL: "http://localhost:8080",
+});
 
-export const retrieveAllTodosForUsernameApi = (username) => apiClient.get(`/users/${username}/todos`)
+export const retrieveAllTodosForUsernameApi = (username) =>
+  apiClient.get(`/users/${username}/todos`);
 
-export const deleteTodoApi = (username, id) => apiClient.delete(`/users/${username}/todos/${id}`)
+export const deleteTodoApi = (username, id) =>
+  apiClient.delete(`/users/${username}/todos/${id}`);
 
-export const retrieveTodoApi = (username, id) => apiClient.get(`/users/${username}/todos/${id}`)
+export const retrieveTodoApi = (username, id) =>
+  apiClient.get(`/users/${username}/todos/${id}`);
 
+export const updateTodoApi = (username, id, todo) =>
+  apiClient.put(`/users/${username}/todos/${id}`, todo);
