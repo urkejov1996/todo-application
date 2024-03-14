110f107f1a4f00a7270f983a90bb58a21f0ff708
urkejov1996 <urkejov1996@gmail.com>
2024-03-14 11:15:58 +0100

    code cleanup

--- src/components/todo/security/AuthContext.jsx
+++ src/components/todo/security/AuthContext.jsx
@@ -16,49 +16,6 @@ export default function AuthProvider({ children }) {
   const [username, setUsername] = useState(null);
   const [token, setToken] = useState(null);
 
-  // function login(username, password) {
-  //     if (username === "urke_jov" && password === "dummy") {
-  //         setAuthenticated(true)
-  //         setUsername(username)
-  //         return true
-  //     } else {
-  //         setAuthenticated(false)
-  //         setUsername(null)
-  //         return false
-  //     }
-  // }
-
-  //   async function login(username, password) {
-
-  //     const baToken = "Basic " + window.btoa(username + ":" + password);
-
-  //     try {
-  //       const response = await executeBasicAuthenticationService(baToken);
-
-  //       if (response.status == 200) {
-  //         setAuthenticated(true);
-  //         setUsername(username);
-  //         setToken(baToken);
-
-  //         apiClient.interceptors.request.use(
-  //             (config) => {
-  //                 console.log('intercepting and adding a token')
-  //                 config.headers.Authorization=baToken
-  //                 return config
-  //             }
-  //         )
-
-  //         return true;
-  //       } else {
-  //         logout();
-  //         return false;
-  //       }
-  //     } catch (error) {
-  //       logout();
-  //       return false;
-  //     }
-  //   }
-
   async function login(username, password) {
     try {
       const response = await executeJWTAuthenticationService(
